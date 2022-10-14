3e699ccf64629919849aa50f052a8ae04dd49576
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-10-14 03:37:55 +0000

    added file

---
+++ guess.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Created by: Lucas LeBlanc 
+# Created on: Oct 2022
+# This program uses if statements to guess a random number
+#    with user input
+
+import constants 
+
+
+def main():
+   # this uses if statements to guess a random number
+
+   # input
+   random_number = int(input("Enter a number between 1 and 9: "))
+
+   # process and output
+   if random_number == constants.CHOSEN_NUMBER:
+       print("You guessed correctly!")
+   if random_number != constants.CHOSEN_NUMBER:
+       print("You guessed incorrectly.")
+
+   print("\nDone.")
+
+
+if __name__ == "__main__":
+   main()
